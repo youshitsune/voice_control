442fa45f23d3a9cc5fdd1b1359643a08e4e651f4
youshitsune <youshitsune@dmz.rs>
2024-02-15 14:42:06 +0100

    inital sketch of program

---
+++ main.py
@@ -0,0 +1,37 @@
+import serial
+import whisper
+import os
+import time
+
+model = whisper.load_model("small")
+options = whisper.DecodingOptions(fp16=False)
+port = serial.Serial("/dev/rfcomm0")
+
+def record():
+    os.system("arecord audio.mp3 -d 3")
+    audio = whisper.load_audio("audio.mp3")
+    audio = whisper.pad_or_trim(audio)
+    os.system("rm audio.mp3")
+    return audio
+
+def stt():
+    global model, options
+    audio = record()
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+    result = whisper.decode(model, mel, options)
+    return result.text.lower()
+
+    
+def run():
+    while True:
+        r = stt()
+        print(r)
+        if r == "napred":
+            port.write(b"%4#4")
+        elif r == "nazad":
+            port.write(b"%7#7")
+
+if __name__ == "__main__":
+    run()
+
+
